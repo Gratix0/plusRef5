d7dddc8eccc9a899898da669a1e7848a5c42821b
Gratix0 <oganisyan.sanya1@mail.ru>
2023-02-16 10:58:57 +0300

    1

---
+++ Recurs/Recurs/Recurs.cpp
@@ -0,0 +1,21 @@
+﻿
+#include<iostream>
+using namespace std;
+int Fact(int n)
+{
+    
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * Fact(n - 1);
+}
+void main()
+{
+
+    int n = 0;
+    cout << "Enter the num: ";
+    cin >> n;
+    int t = Fact(n);
+    cout << "Factorial " << n << " equal " << t;
+    cout << endl;
+}
